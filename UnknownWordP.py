eb627966964e89279f42a7463008081eed0bc45f
Erik Janežič <erik.janezic@gmail.com>
2017-11-24 03:37:24 +0100

    initial commit

---
+++ UnknownWordP.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+class UnknownWordProb:
+      def __init__(self,wordList,SeperatePercentage):
+            self.wordList = wordList
+            self.Seperate = SeperatePercentage
+            
+      def NewWordProb(self):
+            
---
+++ test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+def addercader (x,y):
+      return x+y
